bfb0e211dd12cb68afb196deb9ff24d9e805f1fa
Antonio Ćorluka <corlukantonio@gmail.com>
2020-03-26 05:36:14 +0100

    Hopefully done with backend work
    Hopefully done with backend work.

--- app.py
+++ app.py
@@ -1,26 +1,89 @@
-import db
-import auth
-import chat
-
 import os
 
 from flask import Flask
 from flask_socketio import SocketIO, send
 
+
 app = Flask(__name__)
 socketio = SocketIO(app)
 
-ENV = 'dev'  # if running on localhost set this to 'dev'
 
-if ENV == 'dev':
-    app.debug = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123456@localhost/pywo_chat'
-else:
-    app.debug = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = ''
+# Get the "app" variable
+def get_app():
+    return app
 
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Register blueprints
+def register_blueprints():
+    import auth
+    app.register_blueprint(auth.bp)
+
+    import chat
+    app.register_blueprint(chat.bp)
+    app.add_url_rule('/', endpoint='index')
+
+
+def init_socketio_actions():
+    from db import get_db
+
+    @socketio.on('add_new_contact')
+    def add_new_contact(*json):
+        db = get_db()
+
+        db.session.execute(
+            'INSERT INTO contacts (user_1, user_2) VALUES (:user_1, user_2)',
+            {
+                'user_1': json[1][2],
+                'user_2': json[0]
+            }
+        )
+        db.session.commit()
+
+        db.session.execute(
+            'INSERT INTO contacts (user_1, user_2) VALUES (:user_1, user_2)',
+            {
+                'user_1': json[0],
+                'user_2': json[1][2]
+            }
+        )
+
+    @socketio.on('choose_contact')
+    def choose_contact(*json):
+        pass
+
+    @socketio.on('message')
+    def handle_message(msg, currentUser, targetUser):
+        db = get_db()
+
+        targetUserId = db.session.execute(
+            'SELECT * FROM users WHERE username = :username', {'username': targetUser[2]}).fetchone()
+
+        print('Target user: ' + msg)
+
+        currentUserId = db.session.execute(
+            'SELECT * FROM users WHERE username = :username',
+            {'username': currentUser}
+        ).fetchone()
+
+        print(str(targetUserId['id']) + ' ' + str(currentUserId['id']))
+
+        db.session.execute(
+            'INSERT INTO messages (author_id, sent_to_id, content) VALUES (:author_id, :sent_to_id, :content)',
+            {
+                'author_id': currentUserId['id'],
+                'sent_to_id': targetUserId['id'],
+                'content': msg
+            }
+        )
+        db.session.commit()
+
+        print('Message: ' + msg)
+
+        send([msg, currentUser, targetUser], broadcast=True)
 
 
 if __name__ == '__main__':
+    register_blueprints()
+    init_socketio_actions()
+
     socketio.run(app)
---
+++ auth.py
@@ -0,0 +1,115 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from db import get_db
+
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        db = get_db()
+        error = None
+        username = request.form['username']
+        password = request.form['password']
+        firstname = request.form['firstname']
+        lastname = request.form['lastname']
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif not firstname:
+            error = 'First name is required.'
+        elif not lastname:
+            error = 'Last name is required.'
+        elif db.session.execute(
+            'SELECT id FROM users WHERE username = :username',
+            {'username': username}
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.session.execute(
+                'INSERT INTO users (username, password, firstname, lastname) VALUES (:username, :password, :firstname, :lastname)',
+                {
+                    'username': username,
+                    'password': generate_password_hash(password),
+                    'firstname': firstname,
+                    'lastname': lastname
+                }
+            )
+            db.session.commit()
+
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        db = get_db()
+        error = None
+        username = request.form['username']
+        password = request.form['password']
+
+        user = db.session.execute(
+            'SELECT * FROM users WHERE username = :username',
+            {'username': username}
+        ).fetchone()
+
+        if user is None:
+            error = 'Incorrect username.'
+        elif not check_password_hash(user['password'], password):
+            error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user['id']
+
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    db = get_db()
+    user_id = session.get('user_id')
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = db.session.execute(
+            'SELECT * FROM users WHERE id = :id',
+            {'id': user_id}
+        ).fetchone()
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+
+    return redirect(url_for('index'))
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
