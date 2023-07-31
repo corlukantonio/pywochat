84905c8a59269e3d03ab4505be9a7e25c150cc70
Antonio Ćorluka <corlukantonio@gmail.com>
2023-08-01 00:29:35 +0200

    Update config.py
    - Update config.py.

--- config.py
+++ config.py
@@ -9,5 +9,5 @@ class Config:
     '''
 
     SECRET_KEY = os.environ.get('PYWOCHAT_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('PYWOCHAT_DATABASE_URI')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
